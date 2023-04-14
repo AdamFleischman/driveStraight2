b49b1bbe48b16ab792e6b404c09297ade3496a9e
AdamFleischman <afleischman25@choate.edu>
2023-04-14 09:11:35 -0400

    robot py autonomous work

--- drivestraight.py
+++ drivestraight.py
@@ -1,5 +1,6 @@
 class DriveStraight:
-    def __init__(self,drivetrain):
+    def __init__(self, drivetrain, distance):
         self.drivetrain = drivetrain
+        self.distance = distance
     def run(self):
         pass
